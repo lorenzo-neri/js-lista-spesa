36bd41b44d3535bf51732b5fed432e406149301a
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-30 17:08:57 +0200

    clear code

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,33 +27,5 @@ while (i < shopList.length) {
     i++
 }
 
-/* 
-const ulElement = document.createElement('ul');
-const shoppingList = [];
-let i;
 
-//Inserisco degli elementi all'interno della lista
-shoppingList.push('Frutta');
-shoppingList.push('Pasta');
-shoppingList.push('Verdura');
-shoppingList.push('Sale');
-shoppingList.push('Pepe');
-shoppingList.push('Olio');
-shoppingList.push('Vino');
-shoppingList.push('Acqua');
-console.log(shoppingList);
-
-//Aggiungo gli elementi necessari alla DOM
-bodyElement.append(ulElement);
-
-//Stampo in pagina il contenuto della lista
-i = 0;
-while (i < shoppingList.length) {
-    //Ad ogni iterazione creo un elemento della lista
-    const liElement = document.createElement('li');
-    ulElement.append(liElement);
-    liElement.append(shoppingList[i]);
-    console.log(shoppingList[i]);
-    i++;
-} */
 
